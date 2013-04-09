a4a17e02120114a4c8020588daf32f5c3a20442d
VINAY KUMAR <vinay.kumar.utdallas@gmail.com>
2013-04-09 15:52:18 -0500

    Finite State Machine based web calculator -javascript

---
+++ Finite State Machine Calculator using JavaScript/Finite State Machine Based Calculator/Default.aspx
@@ -0,0 +1,142 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title> 
+   <script src="calc2.js" type="text/javascript"></script>
+    <style type="text/css">
+      
+        #output
+        {
+            width: 304px;
+            text-align: right;
+            border:solid;
+            height: 31px;
+            font-size :x-large;
+        }
+    
+        #clear
+        {
+            width: 309px;
+            background:-moz-linear-gradient(bottom, #0a0809, #0a0f0b 50%, #6d7673 50%, #afbdc0);
+              background:-webkit-gradient(linear, left bottom, left top, from(#0a0809), to(#afbdc0), color-stop(0.5, #0a0f0b), color-stop(0.5, #6d7673));
+              -moz-border-radius:20px;
+              -webkit-border-radius:20px;
+
+              -moz-box-shadow:0px 0px 7px rgba(255, 255,255, 0.5);
+              -webkit-box-shadow:0px 0px 7px rgba(255, 255,255, 0.5);
+
+              /* Text Styling */
+              color:#ff7d00;
+ 
+              font-family:'HighlandGothicFLFRegular', Impact, 'Arial Black', sans-serif;
+              font-size:20px;
+              padding-bottom:5px;
+
+        }
+        
+
+        #button {
+  /* General Properties */
+  height:34px;
+  width:250px;
+  border:1px solid #000;
+  background:#717a77;
+  /* CSS3 Styling */
+  background:-moz-linear-gradient(bottom, #0a0809, #0a0f0b 50%, #6d7673 50%, #afbdc0);
+  background:-webkit-gradient(linear, left bottom, left top, from(#0a0809), to(#afbdc0), color-stop(0.5, #0a0f0b), color-stop(0.5, #6d7673));
+  -moz-border-radius:20px;
+  -webkit-border-radius:20px;
+  border-radius:20px;
+  -moz-box-shadow:0px 0px 7px rgba(255, 255,255, 0.5);
+  -webkit-box-shadow:0px 0px 7px rgba(255, 255,255, 0.5);
+  box-shadow:0px 0px 7px rgba(255, 255,255, 0.5);
+  /* Text Styling */
+  color:#ff7d00;
+  text-shadow:0px -1px 0px #000;
+  font-family:'HighlandGothicFLFRegular', Impact, 'Arial Black', sans-serif;
+  font-size:20px;
+  padding-bottom:5px;
+}
+
+#button:hover {
+  font-size:21px;
+  -moz-box-shadow:0px 0px 7px rgba(255, 255,255, 1);
+  -webkit-box-shadow:0px 0px 7px rgba(255, 255,255, 1);
+  box-shadow:0px 0px 7px rgba(255, 255,255, 1);
+}
+
+#button:focus {
+  -moz-box-shadow:0px 0px 7px rgba(255, 255,255, 0.75);
+  -webkit-box-shadow:0px 0px 7px rgba(255, 255,255, 0.75);
+  box-shadow:0px 0px 7px rgba(255, 255,255, 0.75);
+}
+        #box {
+            height:370px;
+             width:308px;
+             padding-left:10px;
+             padding-right:10px;
+             padding-bottom:25px;
+             padding-top:25px;
+            
+         border:5px solid #000;
+        }
+  
+#button:active {
+  border-width:2px 1px 1px 2px;
+  font-size:20px;
+}
+        #Label1 {
+             text-shadow:0px -1px 0px #000;
+  font-family:'HighlandGothicFLFRegular', Impact, 'Arial Black', sans-serif;
+  font-size:40px;
+        }
+
+    </style>
+    
+</head>
+<body onload="Page_Load()" style="margin-top: 10px">   
+    <form id="form1" runat="server">
+        &nbsp;&nbsp;
+        <asp:Label ID="Label1" runat="server" Text="Calculator and FSM - UTD" style="margin-left: 440px; margin-top: 100px" ></asp:Label>
+    <div id="box" style="margin-left: 489px; margin-top: 30px">
+    <div>   
+        <input id="output" type="text" readonly="readonly" value="0"  />
+   </div>
+    <p>
+        <input id="button" type="button" value="7" style="width: 50px" onclick="btn_Click(7)"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input id="button"type="button" value="8" style="width: 50px" onclick="btn_Click(8)"/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input id="button" type="button" value="9" style="width: 50px" onclick="btn_Click(9)" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input id="button" type="button" value="/" style="width: 50px" onclick="div_Click()"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+    <p>
+        <input id="button" type="button" value="4" style="width: 50px"onclick="btn_Click(4)"/>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input id="button" type="button" value="5" style="width: 50px" onclick="btn_Click(5)"/>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input id="button" type="button" value="6" style="width: 50px" onclick="btn_Click(6)"/>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input id="button" type="button" value="*" style="width: 50px" onclick="mul_Click()" />&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+    <p>
+        <input id="button" type="button" value="1" style="width: 50px" onclick="btn_Click(1)"/>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input id="button" type="button" value="2" style="width: 50px" onclick="btn_Click(2)"/>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input id="button" type="button" value="3" style="width: 50px" onclick="btn_Click(3)"/>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input id="button" type="button" value="+" style="width: 50px" onclick="plus_Click()"/>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+    <p>
+        <input id="button" type="button" value="0" style="width: 50px" onclick="btn_Click(0)"/>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input id="button" type="button" value="." style="width: 50px" onclick="dot_Click()"/>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input id="button" type="button" value="=" style="width: 50px" onclick="eql_Click()"/>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input id="button" type="button" value="-" style="width: 50px" onclick="min_Click()"/>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     </p>
+   
+    <p>
+        <input id="clear" type="button" value="CLEAR" onclick="clr_Click()" /></p>
+   
+   
+   </div>
+        <p>
+            
+        </p>
+    </form>
+    <asp:Image ID="FiniteStateMachine" runat="server" Height="534px" style="margin-left: 125px; border:solid;  margin-right: 195px;" Width="1099px" ImageUrl="~/FiniteStateMachine.jpg" />
+</body>
+</html>
+
---
+++ Finite State Machine Calculator using JavaScript/Finite State Machine Based Calculator/calc2.js
@@ -0,0 +1,323 @@
+﻿var state;
+var firstoperand;
+var first;
+var second;
+var secondoperand;
+var decimalcount;
+var decimal;
+var operation;
+var length;
+//plus     1
+//subtract 2
+//multiply 3
+//divide   4
+
+//WaitForFirstOperand           1
+//FirstOperandBeforeDecimal     2
+//firstOpeandAfterDecimal       3
+//OperandDisplay                4
+//WaitForSecondOperand          5
+//SecondOperandBeforeDecimal    6
+//SecondOperandAfterDecimal     7
+//ShowResult                    8
+
+function Page_Load() {
+    state = 1;
+    firstoperand = 0;
+    length = 10;
+    first = "";
+    second = "";
+    secondoperand = 0;
+    decimalcount = 0;
+    decimal = 0;
+    operation = 0;
+}
+function btn_Click(input) {
+    if (state == 1) {
+        if (input == 0) {
+            //do nothing
+            first = input.toString();
+            document.getElementById("output").value = first;
+        }
+        else {
+            first = first + input.toString();
+            state = 2;
+            document.getElementById("output").value = first;
+        }
+    }
+    else if (state == 2) {
+
+        first = first + input.toString();
+        if (first.length > 10) {
+            document.getElementById("output").value = "Max length " + length;
+            first = "";
+            state = 1;
+        }
+        else {
+
+            document.getElementById("output").value = first;
+        }
+    }
+    else if (state == 3) {
+        decimal = input;
+        decimalcount++; //updating the decimalcount here
+        first = first + decimal.toString();
+        if (first.length > 10) {
+            document.getElementById("output").value = "Max length " + length;
+            first = "";
+            state = 1;
+        }
+        else {
+
+            document.getElementById("output").value = first;
+        }
+
+    }
+    else if (state == 4) {
+        if (input == 0) {
+            state = 5;
+            second = second + input.toString();
+            document.getElementById("output").value = second;
+        }
+        else {
+            second = input.toString();
+            state = 6;
+            second = input.toString();
+            document.getElementById("output").value = second;
+        }
+    }
+    else if (state == 5) {
+        if (input == 0) {
+            document.getElementById("output").value = second;
+        }
+        else {
+            second = input.toString();
+            state = 6;
+            document.getElementById("output").value = second;
+        }
+    }
+    else if (state == 6) {
+        second = second + input.toString();
+        if (second.length > length) {
+            document.getElementById("output").value = "Max length " + length;
+            second = "";
+            state = 5;
+        }
+        else {
+
+            document.getElementById("output").value = second;
+        }
+        
+    }
+    else if (state == 7) {
+        decimal = input.toString();
+        decimalcount++; //updating the decimalcount here
+        second = second + decimal;
+        if (second.length > length) {
+            document.getElementById("output").value = "Max length " + length;
+            second = "";
+            state = 5;
+        }
+        else {
+
+            document.getElementById("output").value = second;
+        }
+        
+    }
+    else if (state == 8) {
+        if (input == 0) {
+            state = 1;
+            first = "";
+            document.getElementById("output").value = "0";
+
+        }
+        else {
+            state = 2;
+            first = input.toString();
+            document.getElementById("output").value = first;
+        }
+    }
+
+}
+function div_Click() {
+    
+    operation = 4;
+    state = 4;
+    decimalcount = 0;
+    decimal = 0;
+    document.getElementById("output").value = "/";
+
+
+}
+
+function mul_Click() {
+    operation = 3;
+    state = 4;
+    decimalcount = 0;
+    decimal = 0;
+    document.getElementById("output").value = "*";
+
+}
+function clr_Click() {
+    operation = 0;
+    state = 1;
+    decimalcount = 0;
+    decimal = 0;
+    first = "";
+    second = "";
+    document.getElementById("output").value = "0";
+}
+function plus_Click() {
+    operation = 1;
+    state = 4;
+    decimalcount = 0;
+    decimal = 0;
+    document.getElementById("output").value = "+";
+}
+function min_Click() {
+    operation = 2;
+    state = 4;
+    decimalcount = 0;
+    decimal = 0;
+    document.getElementById("output").value = "-";
+
+}
+function eql_Click() {
+    state = 8;
+    decimalcount = 0;
+    firstoperand = 0;
+    secondoperand = 0;
+    decimal = 0;
+    if (operation == 1) {
+        if (first=="") {
+            first = "0";
+        }
+        firstoperand = parseFloat(first) + parseFloat(second);
+        first = firstoperand.toString();      
+        second = "";
+        if (isNaN(firstoperand)) {
+            document.getElementById("output").value = "Syntax Error";
+        }
+        else {
+            document.getElementById("output").value = firstoperand;
+        }
+        operation = 0;
+    }
+    else if (operation == 2) {
+        if (first == "") {
+            first = "0";
+        }
+        firstoperand = parseFloat(first) - parseFloat(second);
+        first = firstoperand.toString();
+        second = "";
+        if (isNaN(firstoperand)) {
+            document.getElementById("output").value = "Syntax Error";
+        }
+        else {
+            document.getElementById("output").value = firstoperand;
+        }
+        
+        operation = 0;
+    }
+    else if (operation == 3) {
+        if (first == "") {
+            first = "0";
+        }
+        firstoperand = parseFloat(first) * parseFloat(second);
+        first = firstoperand.toString();
+        second = "";
+        if (isNaN(firstoperand)) {
+            document.getElementById("output").value = "Syntax Error";
+        }
+        else {
+            document.getElementById("output").value = firstoperand;
+        }
+   
+        operation = 0;
+    }
+    else if (operation == 4) {
+        if (first == "") {
+            first = "0";
+        }
+        firstoperand = parseFloat(first) / parseFloat(second);
+        first = firstoperand.toString();
+        second = "";
+        if (isNaN(firstoperand)) {
+            document.getElementById("output").value = "Syntax Error";
+        }
+        else {
+            document.getElementById("output").value = firstoperand;
+        }
+        
+        operation = 0;
+    }
+
+
+
+}
+function dot_Click() {
+    if (state == 1) {
+        state = 3;
+
+        first = first + ".";
+        if (first == ".") {
+            first = "0.";
+        }
+        document.getElementById("output").value = first;
+        first
+    }
+    if (state == 2) {
+        state = 3;
+        first = first + ".";
+        if (first == ".") {
+            first = "0.";
+        }
+        document.getElementById("output").value = first;
+    }
+    if (state == 3) {
+        state = 3;
+    }
+    if (state == 4) {
+        document.getElementById("output").value = state;
+        state = 7;
+        second = second + ".";
+        if (second == ".") {
+            second = "0.";
+        }
+
+
+    }
+    if (state == 5) {
+        state = 7;
+        second = second + ".";
+        if (second == ".") {
+            second = "0.";
+        }
+        document.getElementById("output").value = second;
+    }
+    if (state == 6) {
+        state = 7;
+        second = second + ".";
+        if (second == ".") {
+            second = "0.";
+        }
+        document.getElementById("output").value = second;
+    }
+    if (state == 7) {
+        state = 7;
+    }
+
+    if (state == 8) {
+        state = 3;
+        first = "0";
+        firstoperand = 0;
+        first = first + ".";
+        if (first == ".") {
+            first = "0.";
+        }
+        document.getElementById("output").value = first;
+
+    }
+
+
+}
